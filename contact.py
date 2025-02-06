6006e7ac8f3e1c2808ce5e9847169709df3346c3
Mari Kathleen Abdon <kathabdon06@gmail.com>
2025-02-06 18:52:22 -0500

    added a subclass BusinessContact inheriting from Contact class

--- contact.py
+++ contact.py
@@ -20,7 +20,28 @@ class Contact:
       
   def changeNumber(self, number):
       self.__number = number
-      
 
+class BusinessContact(Contact): #Inheritance
+  def __init__(self, name, number, email, company):
+     super().__init__(name, number)
+     self.__email = email
+     self.__company = company
+  
+  #getters
+  def getEmail(self):
+    return self.__email
+  
+  def getCompany(self):
+    return self.__company
+  
+  #setters
+  def changeEmail(self, email):
+    self.__email = email
+    
+  def changeCompany(self, company):
+    self.__company = company
+    
+     
+  
 
 
